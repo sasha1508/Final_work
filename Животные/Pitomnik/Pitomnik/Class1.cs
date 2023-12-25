860720e18caab8926d0d5b953b18ca1dd265dc54
Александр Ильин <sasha8@list.ru>
2023-12-25 21:30:43 +0300

    Сделано добавление животных

--- Животные/Pitomnik/Pitomnik/Class1.cs
+++ Животные/Pitomnik/Pitomnik/Class1.cs
@@ -6,7 +6,176 @@ using System.Threading.Tasks;
 
 namespace Pitomnik
 {
-    internal class Class1
+    public class Animals
     {
+        public string name = "";
+        public string birthDate = "";
+        public string commands = "";
+    }
+
+    public class Pets : Animals
+    {
+        public string coatColor = "";
+    }
+
+    public class Dog : Pets 
+    {
+        public Dog(string name, string birthDate, string commands, string coatColor) 
+        {
+            this.name = name;
+            this.birthDate = birthDate;
+            this.commands = commands;
+            this.coatColor = coatColor;
+        }
+
+        public static Dog AddDog()
+        {
+            Console.WriteLine("Введи кличку собаки");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введи дату рождения собаки");
+            string birthDate = Console.ReadLine();
+            Console.WriteLine("Введи исполняемые собакой команды");
+            string commands = Console.ReadLine();
+            Console.WriteLine("Введи цвет шерсти собаки");
+            string coatColor = Console.ReadLine();
+
+            Dog dog = new(name, birthDate, commands, coatColor);
+            return dog;
+        }
+    }
+
+    public class Cat : Pets
+    {
+        public Cat(string name, string birthDate, string commands, string coatColor)
+        {
+            this.name = name;
+            this.birthDate = birthDate;
+            this.commands = commands;
+            this.coatColor = coatColor;
+        }
+
+        public static Cat AddCat()
+        {
+            Console.WriteLine("Введи кличку кошки");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введи дату рождения кошки");
+            string birthDate = Console.ReadLine();
+            Console.WriteLine("Введи исполняемые кошкой команды");
+            string commands = Console.ReadLine();
+            Console.WriteLine("Введи цвет шерсти кошки");
+            string coatColor = Console.ReadLine();
+
+            Cat cat = new(name, birthDate, commands, coatColor);
+            return cat;
+        }
+    }
+
+    public class Hamster : Pets
+    {
+        public Hamster(string name, string birthDate, string commands, string coatColor)
+        {
+            this.name = name;
+            this.birthDate = birthDate;
+            this.commands = commands;
+            this.coatColor = coatColor;
+        }
+
+        public static Hamster AddHamster()
+        {
+            Console.WriteLine("Введи кличку хомячка");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введи дату рождения хомячка");
+            string birthDate = Console.ReadLine();
+            Console.WriteLine("Введи исполняемые хомячком команды");
+            string commands = Console.ReadLine();
+            Console.WriteLine("Введи цвет шерсти хомячка");
+            string coatColor = Console.ReadLine();
+
+            Hamster hamster = new(name, birthDate, commands, coatColor);
+            return hamster;
+        }
+    }
+
+    public class PackAnimals : Animals
+    {
+        public string works = "";
+    }
+
+    public class Horse : PackAnimals 
+    {
+        public Horse(string name, string birthDate, string commands, string works)
+        {
+            this.name = name;
+            this.birthDate = birthDate;
+            this.commands = commands;
+            this.works = works;
+        }
+
+        public static Horse AddHorse()
+        {
+            Console.WriteLine("Введи кличку лошади");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введи дату рождения лошади");
+            string birthDate = Console.ReadLine();
+            Console.WriteLine("Введи исполняемые лошадью команды");
+            string commands = Console.ReadLine();
+            Console.WriteLine("Введи выполняемые лошадью работы");
+            string works = Console.ReadLine();
+
+            Horse horse = new(name, birthDate, commands, works);
+            return horse;
+        }
+    }
+
+    public class Camel : PackAnimals
+    {
+        public Camel(string name, string birthDate, string commands, string works)
+        {
+            this.name = name;
+            this.birthDate = birthDate;
+            this.commands = commands;
+            this.works = works;
+        }
+
+        public static Camel AddCamel()
+        {
+            Console.WriteLine("Введи кличку верблюда");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введи дату рождения верблюда");
+            string birthDate = Console.ReadLine();
+            Console.WriteLine("Введи исполняемые верблюдом команды");
+            string commands = Console.ReadLine();
+            Console.WriteLine("Введи выполняемые верблюдом работы");
+            string works = Console.ReadLine();
+
+            Camel camel = new(name, birthDate, commands, works);
+            return camel;
+        }
+    }
+
+    public class Donkey : PackAnimals
+    {
+        public Donkey(string name, string birthDate, string commands, string works)
+        {
+            this.name = name;
+            this.birthDate = birthDate;
+            this.commands = commands;
+            this.works = works;
+        }
+
+        public static Donkey AddDonkey()
+        {
+            Console.WriteLine("Введи кличку осла");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введи дату рождения осла");
+            string birthDate = Console.ReadLine();
+            Console.WriteLine("Введи исполняемые ослом команды");
+            string commands = Console.ReadLine();
+            Console.WriteLine("Введи выполняемые ослом работы");
+            string works = Console.ReadLine();
+
+            Donkey donkey = new(name, birthDate, commands, works);
+            return donkey;
+        }
     }
 }
--- Животные/Pitomnik/Pitomnik/Program.cs
+++ Животные/Pitomnik/Pitomnik/Program.cs
@@ -1,14 +1,43 @@
 ﻿
 
+using Pitomnik;
+using System.Xml.Linq;
+
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("ss");
+        List<Animals> Animals = new();
+        string input = " ";
+        while(!input.Equals("1") && !input.Equals("2") && !input.Equals("3") 
+            && !input.Equals("4") && !input.Equals("5") && !input.Equals("6"))
+        {
+            input = read1();
+        }
+
+        if (input == "1") Animals.Add(Dog.AddDog());
+        if (input == "2") Animals.Add(Cat.AddCat());
+        if (input == "3") Animals.Add(Hamster.AddHamster());
+        if (input == "4") Animals.Add(Horse.AddHorse());
+        if (input == "5") Animals.Add(Camel.AddCamel());
+        if (input == "6") Animals.Add(Donkey.AddDonkey());
+    }
+
+    static public string read1()
+    {
+        Console.Clear();
+        Console.WriteLine("Добавление животного.");
+        Console.WriteLine("1 - Собака.");
+        Console.WriteLine("2 - Кошка.");
+        Console.WriteLine("3 - Хомяк.");
+        Console.WriteLine("4 - Лошадь.");
+        Console.WriteLine("5 - Верблюд.");
+        Console.WriteLine("6 - Осёл.");
+        string read = "";
+        read =  Console.ReadLine();
+        return read;
     }
 }
 
 
 
-// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
