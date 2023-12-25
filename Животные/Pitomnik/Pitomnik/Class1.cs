9ebaad5065af0f8a43d5af08dcfc1c91369f44d1
Александр Ильин <sasha8@list.ru>
2023-12-25 23:05:21 +0300

    Выполнено

--- Животные/Pitomnik/Pitomnik/Class1.cs
+++ Животные/Pitomnik/Pitomnik/Class1.cs
@@ -8,9 +8,15 @@ namespace Pitomnik
 {
     public class Animals
     {
+        public string type = "";
         public string name = "";
         public string birthDate = "";
         public string commands = "";
+
+        public void print() 
+        {
+            Console.WriteLine("- " + type + ". Кличка: " + name + ". Дата рождения: " + birthDate + ". Выполняемые команды: " + commands + ".");
+        }
     }
 
     public class Pets : Animals
@@ -22,25 +28,33 @@ namespace Pitomnik
     {
         public Dog(string name, string birthDate, string commands, string coatColor) 
         {
+            type = "Собака";
             this.name = name;
             this.birthDate = birthDate;
             this.commands = commands;
             this.coatColor = coatColor;
         }
 
+        //Создание экземпляра собаки:
         public static Dog AddDog()
         {
-            Console.WriteLine("Введи кличку собаки");
+            Console.WriteLine("Введи кличку собаки:");
             string name = Console.ReadLine();
-            Console.WriteLine("Введи дату рождения собаки");
+            Console.WriteLine("Введи дату рождения собаки (Год.Месяц.Число):");
             string birthDate = Console.ReadLine();
-            Console.WriteLine("Введи исполняемые собакой команды");
+            Console.WriteLine("Введи исполняемые собакой команды:");
             string commands = Console.ReadLine();
-            Console.WriteLine("Введи цвет шерсти собаки");
+            Console.WriteLine("Введи цвет шерсти собаки:");
             string coatColor = Console.ReadLine();
 
             Dog dog = new(name, birthDate, commands, coatColor);
             return dog;
+        }
+        
+        //Вывод на экран клички и команд:
+        public new void print() 
+        { 
+            Console.WriteLine("Собака. Кличка: " + name + ". Выполняемые команды: " + commands + ".");
         }
     }
 
@@ -48,6 +62,7 @@ namespace Pitomnik
     {
         public Cat(string name, string birthDate, string commands, string coatColor)
         {
+            type = "Кошка";
             this.name = name;
             this.birthDate = birthDate;
             this.commands = commands;
@@ -56,13 +71,13 @@ namespace Pitomnik
 
         public static Cat AddCat()
         {
-            Console.WriteLine("Введи кличку кошки");
+            Console.WriteLine("Введи кличку кошки:");
             string name = Console.ReadLine();
-            Console.WriteLine("Введи дату рождения кошки");
+            Console.WriteLine("Введи дату рождения кошки (Год.Месяц.Число):");
             string birthDate = Console.ReadLine();
-            Console.WriteLine("Введи исполняемые кошкой команды");
+            Console.WriteLine("Введи исполняемые кошкой команды:");
             string commands = Console.ReadLine();
-            Console.WriteLine("Введи цвет шерсти кошки");
+            Console.WriteLine("Введи цвет шерсти кошки:");
             string coatColor = Console.ReadLine();
 
             Cat cat = new(name, birthDate, commands, coatColor);
@@ -74,6 +89,7 @@ namespace Pitomnik
     {
         public Hamster(string name, string birthDate, string commands, string coatColor)
         {
+            type = "Хомяк";
             this.name = name;
             this.birthDate = birthDate;
             this.commands = commands;
@@ -82,13 +98,13 @@ namespace Pitomnik
 
         public static Hamster AddHamster()
         {
-            Console.WriteLine("Введи кличку хомячка");
+            Console.WriteLine("Введи кличку хомячка:");
             string name = Console.ReadLine();
-            Console.WriteLine("Введи дату рождения хомячка");
+            Console.WriteLine("Введи дату рождения хомячка (Год.Месяц.Число):");
             string birthDate = Console.ReadLine();
-            Console.WriteLine("Введи исполняемые хомячком команды");
+            Console.WriteLine("Введи исполняемые хомячком команды:");
             string commands = Console.ReadLine();
-            Console.WriteLine("Введи цвет шерсти хомячка");
+            Console.WriteLine("Введи цвет шерсти хомячка:");
             string coatColor = Console.ReadLine();
 
             Hamster hamster = new(name, birthDate, commands, coatColor);
@@ -105,6 +121,7 @@ namespace Pitomnik
     {
         public Horse(string name, string birthDate, string commands, string works)
         {
+            type = "Лошадь";
             this.name = name;
             this.birthDate = birthDate;
             this.commands = commands;
@@ -113,13 +130,13 @@ namespace Pitomnik
 
         public static Horse AddHorse()
         {
-            Console.WriteLine("Введи кличку лошади");
+            Console.WriteLine("Введи кличку лошади:");
             string name = Console.ReadLine();
-            Console.WriteLine("Введи дату рождения лошади");
+            Console.WriteLine("Введи дату рождения лошади (Год.Месяц.Число):");
             string birthDate = Console.ReadLine();
-            Console.WriteLine("Введи исполняемые лошадью команды");
+            Console.WriteLine("Введи исполняемые лошадью команды:");
             string commands = Console.ReadLine();
-            Console.WriteLine("Введи выполняемые лошадью работы");
+            Console.WriteLine("Введи выполняемые лошадью работы:");
             string works = Console.ReadLine();
 
             Horse horse = new(name, birthDate, commands, works);
@@ -131,6 +148,7 @@ namespace Pitomnik
     {
         public Camel(string name, string birthDate, string commands, string works)
         {
+            type = "Верблюд";
             this.name = name;
             this.birthDate = birthDate;
             this.commands = commands;
@@ -139,13 +157,13 @@ namespace Pitomnik
 
         public static Camel AddCamel()
         {
-            Console.WriteLine("Введи кличку верблюда");
+            Console.WriteLine("Введи кличку верблюда:");
             string name = Console.ReadLine();
-            Console.WriteLine("Введи дату рождения верблюда");
+            Console.WriteLine("Введи дату рождения верблюда (Год.Месяц.Число):");
             string birthDate = Console.ReadLine();
-            Console.WriteLine("Введи исполняемые верблюдом команды");
+            Console.WriteLine("Введи исполняемые верблюдом команды:");
             string commands = Console.ReadLine();
-            Console.WriteLine("Введи выполняемые верблюдом работы");
+            Console.WriteLine("Введи выполняемые верблюдом работы:");
             string works = Console.ReadLine();
 
             Camel camel = new(name, birthDate, commands, works);
@@ -157,6 +175,7 @@ namespace Pitomnik
     {
         public Donkey(string name, string birthDate, string commands, string works)
         {
+            type = "Осёл";
             this.name = name;
             this.birthDate = birthDate;
             this.commands = commands;
@@ -165,13 +184,13 @@ namespace Pitomnik
 
         public static Donkey AddDonkey()
         {
-            Console.WriteLine("Введи кличку осла");
+            Console.WriteLine("Введи кличку осла:");
             string name = Console.ReadLine();
-            Console.WriteLine("Введи дату рождения осла");
+            Console.WriteLine("Введи дату рождения осла (Год.Месяц.Число):");
             string birthDate = Console.ReadLine();
-            Console.WriteLine("Введи исполняемые ослом команды");
+            Console.WriteLine("Введи исполняемые ослом команды:");
             string commands = Console.ReadLine();
-            Console.WriteLine("Введи выполняемые ослом работы");
+            Console.WriteLine("Введи выполняемые ослом работы:");
             string works = Console.ReadLine();
 
             Donkey donkey = new(name, birthDate, commands, works);
--- Животные/Pitomnik/Pitomnik/Program.cs
+++ Животные/Pitomnik/Pitomnik/Program.cs
@@ -1,26 +1,91 @@
 ﻿
 
 using Pitomnik;
+using System.Collections.Generic;
 using System.Xml.Linq;
 
 class Program
 {
     static void Main()
     {
-        List<Animals> Animals = new();
+        List<Animals> animals = new();
         string input = " ";
-        while(!input.Equals("1") && !input.Equals("2") && !input.Equals("3") 
+
+        while (!input.Equals("q"))
+        {
+            Console.Clear();
+            Console.WriteLine("q - выход из программы");
+            Console.WriteLine("");
+            Console.WriteLine("1 - добавить животное");
+            Console.WriteLine("2 - обучить новым командам");
+            Console.WriteLine("3 - вывести список животных по дате рождения");
+            Console.WriteLine("");
+            Console.WriteLine("Всего животных: " + animals.Count);
+            foreach (Animals animal in animals)
+            {
+                animal.print();
+            }
+            input = Console.ReadLine();
+            if (input.Equals("q")) return;  //выход
+            if (input.Equals("1")) addAnimal(animals);  //добавляем в список новое животное
+            if (input.Equals("2")) addCommand(animals);  //обучаем новым командам
+            if (input.Equals("3")) sortOfBirthData(animals);  //выводим список животных по дате рождения
+        }
+    }
+
+    static void sortOfBirthData(List<Animals> animals)
+    {
+        foreach (Animals animal in animals.OrderBy(x => x.birthDate))
+        {
+            animal.print();
+        }
+        Console.Read();
+    }
+
+    static void addCommand(List<Animals> animals) 
+    {
+        string input = " ";
+        int inputInt = 0;
+        while (!input.Equals("q"))
+        {
+            Console.Clear();
+            Console.WriteLine("q - выход в главное меню");
+            Console.WriteLine("");
+            Console.WriteLine("Введи число соответствующее требуемому животному:");
+            int counter = 1;
+            foreach (var animal in animals)
+            {
+                Console.Write(counter.ToString() + " ");
+                animal.print();
+                counter++;
+            }
+            input = Console.ReadLine();
+            try
+            {
+                inputInt = Convert.ToInt32(input);
+                animals[inputInt - 1].print();
+                Console.WriteLine("Введи список новых команд:");
+                animals[inputInt - 1].commands += ", " + Console.ReadLine();
+            }
+            catch { }
+        }
+    }
+
+    static void addAnimal(List<Animals> animals)
+    {
+        string input = " ";
+        while (!input.Equals("1") && !input.Equals("2") && !input.Equals("3")
             && !input.Equals("4") && !input.Equals("5") && !input.Equals("6"))
         {
             input = read1();
         }
 
-        if (input == "1") Animals.Add(Dog.AddDog());
-        if (input == "2") Animals.Add(Cat.AddCat());
-        if (input == "3") Animals.Add(Hamster.AddHamster());
-        if (input == "4") Animals.Add(Horse.AddHorse());
-        if (input == "5") Animals.Add(Camel.AddCamel());
-        if (input == "6") Animals.Add(Donkey.AddDonkey());
+        if (input == "1") animals.Add(Dog.AddDog());
+        if (input == "2") animals.Add(Cat.AddCat());
+        if (input == "3") animals.Add(Hamster.AddHamster());
+        if (input == "4") animals.Add(Horse.AddHorse());
+        if (input == "5") animals.Add(Camel.AddCamel());
+        if (input == "6") animals.Add(Donkey.AddDonkey());
     }
 
     static public string read1()
